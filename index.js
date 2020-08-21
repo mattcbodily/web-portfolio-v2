eb70d62df5fc262adad78c3f0faedf61cadfb180
mattcbodily <mattcbodily@gmail.com>
2020-08-21 15:16:37 -0600

    added responsive styling to the project cards

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//skills section
 const baseUrl = './assets/skill-assets/',
       htmlIcon = {url: `${baseUrl}html5.png`, alt: 'HTML'},
       cssIcon = {url: `${baseUrl}css3.png`, alt: 'CSS'},
@@ -30,4 +31,35 @@ for(let i = 0; i < skillsArr.length; i++){
     container.appendChild(skillImage);
     container.appendChild(skillText);
     skillsFlex.appendChild(container);
+}
+
+//projects section
+const projectURL = './assets/project-assets/',
+      projects = [
+        {name: 'Chatter', image: `${projectURL}chatter-portfolio.png`, description: 'Chat application built with React, Redux, Sass, Node, Express, and PostgreSQL'},
+        {name: 'Pokédex', image: `${projectURL}pokedex-portfolio.png`, description: 'Pokédex application built with React (including React hooks), and the PokeAPI'},
+        {name: 'Arcade', image: `${projectURL}arcade-portfolio.png`, description: "Arcade is where I store games I've built using HTML, CSS, and JavaScript."}
+      ]
+
+for(let j = 0; j < projects.length; j++){
+    const projectDisplay = document.querySelector('.project-display'),
+          projectCard = document.createElement('div'),
+          projectImage = document.createElement('img'),
+          projectTextContainer = document.createElement('div'),
+          projectText =  document.createElement('p');
+
+    projectCard.classList.add('project-card');
+    projectImage.classList.add('project-image');
+    projectTextContainer.classList.add('project-text-container');
+    projectText.classList.add('project-text');
+
+    projectImage.src = projects[j].image;
+    projectImage.alt = projects[j].name;
+    projectCard.appendChild(projectImage);
+
+    projectText.innerText = projects[j].description;
+    projectTextContainer.appendChild(projectText);
+    projectCard.appendChild(projectTextContainer);
+
+    projectDisplay.appendChild(projectCard);
 }
